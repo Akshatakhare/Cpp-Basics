4e2f0695b70b11adfc4a78c6722bc9d7d7c81a5a
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-11 01:19:32 +0530

    op

---
+++ operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // operators
+    // int a, b;
+    // cout<<"Enter First number : ";
+    // cin>>a;
+    // cout<<"Enter Second number: ";
+    // cin>>b;
+
+    // cout<<"Addition of two no: "<< a+b<<endl;
+    // cout<<"Subtraction of two no: "<< a-b<<endl;
+    // cout<<"Multiplication of two no: "<< a*b<<endl;
+    // cout<<"Division of two no: "<< a/b<<endl;
+    // cout<<"Division of two no: "<< (float) a/b<<endl;
+    // cout<<"Remainder of two no: "<< a%b<<endl;
+    // // cout<<"Addition of two no: "<< a**b;
+
+    int x= 10;
+    x+=5;
+    cout<<x;
+// relational
+
+    int a = 4, b = 5;
+    cout << "The value of a == b is " << (a == b) << endl;
+    cout << "The value of a < b is " << (a < b) << endl;
+    cout << "The value of a > b is " << (a > b) << endl;
+
+}
