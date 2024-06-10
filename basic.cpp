ae41d752900a29941fa8543940e7c50c8ace622a
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-10 22:13:53 +0530

    Basic syntax

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    cout<<"Toh kese h aap log?"<<endl;
+    
+
+    int a=78;
+    cout<<a;
+    return 0;
+}
