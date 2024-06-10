ec845474a391cc06e08818c7e68839a8e43871fe
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-10 22:28:10 +0530

    variable

---
+++ variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int marksInEnglish = 48;
+    // cout<<"The total marks out of 50 : "<<marksInEnglish<<endl;
+    
+    // int age=20;
+    // age=21;
+    // cout<<age<<endl;
+    // float b=789.456;
+    // char c='a';
+    // bool e=true;
+    // string babygirl="Hi, baby girl!";
+    // cout<<babygirl;
+
+    int a, b, c;
+    cout<<"Enter the value : ";
+    cin>>a;
+}
