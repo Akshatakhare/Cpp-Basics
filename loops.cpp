d1c1291d8b2aa4be5941b31f1c7c2e93e9719f64
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-12 00:08:59 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // print 1-10 natural numbers:
+
+    // int num=1;
+    // while (num<21)
+    // {
+    //     cout<<num<<endl;
+    //     num=num+1;
+    // }
+
+//     int num=10;
+//     do
+//     {
+//         cout<<num<<endl;
+//         num=num+1;
+//     } while (num>100);
+    
+
+    for (int i = 1; i <= 30; i++)
+    {
+        cout<<"All natural numbers are : "<<i<<endl;
+    }
+    
+    return 0;
+}
